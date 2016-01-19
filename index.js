51efabf6aee4a4cdfff1739ec0202325bee6b9c9
johnsoja40 <james40johnson@gmail.com>
2016-01-19 15:58:47 -0500

    added node.js files

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Serving Static Files");
+
+var connect = require('connect');
+
+var serveStatic = require('serve-static');
+
+var app = connect()
+	.use(serveStatic('myPublicFolder'))
+	.use(function(req,res){
+		res.end("Welcome to our demo app.");	
+	})
+	.listen(8080);
+console.log("Listening on port 8080");
